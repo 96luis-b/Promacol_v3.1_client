069e87e10d0a68e1f2d9bc6e1142fe5d9571305b
96luis-b <luisbocaranda19@gmail.com>
2022-11-21 17:43:25 -0400

    x-access-token para el envio de token al servidor

--- src/api/price.js
+++ src/api/price.js
@@ -2,26 +2,28 @@ import api from '../api/server'
 
 
 export const getExchangeRate = async (data) => {
-    const res = await fetch(`${api}price/getExchangeRate`, {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      });
-      return res.json();
+  const res = await fetch(`${api}price/getExchangeRate`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  return res.json();
 }
 
 export const updateExchangeRate = async (data) => {
-    const res = await fetch(`${api}price/updateExchangeRate`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    return res.json();
+  let storage = JSON.parse(localStorage.getItem('user'))
+  const res = await fetch(`${api}price/updateExchangeRate`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "x-access-token": `${storage.token}`
+    },
+    body: JSON.stringify(data),
+  });
+  return res.json();
 }
 
 
