52f774ad6b03309871a58b984a4dc0998bc2f99c
96luis-b <luisbocaranda19@gmail.com>
2023-01-02 12:48:48 -0400

    se agregan celdas para contener: total_dollar, pay_bs y pay_dollar

--- src/components/Modals/PayrollEmployeeReport.js
+++ src/components/Modals/PayrollEmployeeReport.js
@@ -15,6 +15,7 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
 export default function PayrollEmployeeReport({ open, handleOpen, handleClose, data, datePayroll, timePayroll, time, date }) {
+    console.log("data: ", data)
     return (
         <div>
             <Dialog
@@ -47,43 +48,45 @@ export default function PayrollEmployeeReport({ open, handleOpen, handleClose, d
                                     <TableCell align="center" colSpan={2}>Producto</TableCell>
                                     <TableCell align="center">Total Unidades</TableCell>
                                     <TableCell align="center">Total Bs</TableCell>
+                                    <TableCell align="center">Total $</TableCell>
+                                    <TableCell align="center">Bs pagados</TableCell>
+                                    <TableCell align="center">$ pagados</TableCell>
                                 </TableRow>
                             </TableHead>
                             {/* <TableBody> */}
-                                {data.map((row, i) => {
-                                    return <TableBody key={i}>
-                                        {
-                                            row.payroll.map((r, j) => {
-                                                let total_bs = 0, total_value = 0;
-                                                return <>
-                                                    <TableRow
-                                                        key={j}
-                                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                                    >
-                                                        <TableCell align="right">{j + 1}</TableCell>
-                                                        <TableCell align="right">{r.date}</TableCell>
-                                                        <TableCell align="right">{row.job_name}</TableCell>
-                                                        <TableCell align="right">{row.emp_code}</TableCell>
-                                                        <TableCell align="right">{`${row.name1} ${row.name2} ${row.lastname1} ${row.lastname2}`}</TableCell>
-                                                        <TableCell align="right">{row.id_number}</TableCell>
-                                                        {r.payroll_detail.map((prod, z) => {
-                                                            console.log("total_bs: ", total_bs)
-                                                            console.log("prod.total_bs: ", prod.total_bs)
-                                                            total_bs = parseFloat(total_bs) + parseFloat(prod.total_bs)
-                                                            total_value = total_value + prod.quantity
-                                                            return <TableCell align="right" key={z}>{`${prod.prod_name}: ${prod.quantity}`}</TableCell>
-                                                        })
-                                                        }
-                                                        <TableCell align="right">{total_value}</TableCell>
-                                                        <TableCell align="right">{total_bs}</TableCell>
+                            {data.map((row, i) => {
+                                return <TableBody key={i}>
+                                    {
+                                        row.payroll.map((r, j) => {
+                                            let total_bs = 0, total_value = 0, total_dollar = 0, pay_bs = 0, pay_dollar = 0;
+                                            return <TableRow
+                                                key={j}
+                                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                            >
+                                                <TableCell align="right">{j + 1}</TableCell>
+                                                <TableCell align="right">{r.date}</TableCell>
+                                                <TableCell align="right">{row.job_name}</TableCell>
+                                                <TableCell align="right">{row.emp_code}</TableCell>
+                                                <TableCell align="right">{`${row.name1} ${row.name2} ${row.lastname1} ${row.lastname2}`}</TableCell>
+                                                <TableCell align="right">{row.id_number}</TableCell>
+                                                {r.payroll_detail.map((prod, z) => {
+                                                    total_bs = parseFloat(total_bs) + parseFloat(prod.total_bs)
+                                                    total_value = total_value + prod.quantity
+                                                    return <TableCell align="right" key={z}>{`${prod.prod_name}: ${prod.quantity}`}</TableCell>
+                                                })
+                                                }
+                                                <TableCell align="right">{total_value}</TableCell>
+                                                <TableCell align="right">{parseFloat(r.total_bs).toFixed(2)}</TableCell>
+                                                <TableCell align="right">{parseFloat(r.total_dollar).toFixed(2)}</TableCell>
+                                                <TableCell align="right">{parseFloat(r.pay_bs).toFixed(2)}</TableCell>
+                                                <TableCell align="right">{parseFloat(r.pay_dollar).toFixed(2)}</TableCell>
 
-                                                    </TableRow>
-                                                </>
-                                            })
-                                        }
-                                    </TableBody>
-                                })
-                                }
+                                            </TableRow>
+                                        })
+                                    }
+                                </TableBody>
+                            })
+                            }
                         </Table>
                     </TableContainer>
                 </DialogContent>
