bc30ef474992652eea4997fa69ec14f44f6d4319
96luis-b <luisbocaranda19@gmail.com>
2023-01-07 07:22:44 -0400

    dialog para editar unidades de produccion al personal de destajo

---
+++ src/components/Modals/ProdJobDialog.js
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import { DialogContent, DialogContentText, DialogActions, ListItemButton, ListItemIcon, ListItemText, Grid } from '@mui/material';
+import { H2, H3, H4, H5 } from '../../styledComponents/Heading';
+import * as GoIcons from 'react-icons/go'
+import { TextFieldSelect } from '../TextFieldSelect';
+import * as AiIcons from 'react-icons/ai'
+
+
+export default function ProdJobDialog({
+     open, 
+     handleClickOpen, 
+     handleClose, 
+     data, 
+     options, 
+     option, 
+     handleChange, 
+     handleOpenConfirm, 
+     handleAddProdJob,
+     handleRemoveProdJob
+    }) {
+
+
+
+    return (
+        <>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+            >
+                <DialogTitle>
+                    <H3>Productos por puesto de trabajo</H3>
+                </DialogTitle>
+                <DialogContent>
+                    <H5>{data.job_name}</H5>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        {data.products.map((prod, j) => {
+                            return <ListItemButton key={j}>
+                                <ListItemIcon>
+                                    <GoIcons.GoPrimitiveDot />
+                                </ListItemIcon>
+                                <ListItemText primary={`${prod.prod_name}`} />
+                                <ListItemIcon sx={{ color: "red", fontSize: "20px" }} onClick={()=>handleRemoveProdJob(data, prod)}>
+                                    <AiIcons.AiFillDelete />
+                                </ListItemIcon>
+                            </ListItemButton>
+                        })
+                        }
+                    </DialogContentText>
+                    <Grid container my={4}>
+                        <Grid item xs={8}>
+                            {/* <TextFieldSelect /> */}
+                            <TextFieldSelect
+                                options={options}
+                                onChange={handleChange}
+                                option={option}
+                                width={"90%"}
+                                label={"Unidades de producción"} />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Button variant="contained" onClick={()=> handleAddProdJob(data, option)}>Agregar</Button>
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Disagree</Button>
+                    <Button onClick={handleClose}>Agree</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+}
